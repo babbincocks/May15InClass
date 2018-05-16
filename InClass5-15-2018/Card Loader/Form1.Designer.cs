611cdfd599124a0689a0753da3aa457a15b80ef4
babbincocks <zabbybab@gmail.com>
2018-05-15 21:00:48 -0400

    Card Generator thing works, hooray.

---
+++ InClass5-15-2018/Card Loader/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Card_Loader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ilCards = new System.Windows.Forms.ImageList(this.components);
+            this.lbCards = new System.Windows.Forms.ListBox();
+            this.pbSelectedCard = new System.Windows.Forms.PictureBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelectedCard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ilCards
+            // 
+            this.ilCards.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilCards.ImageStream")));
+            this.ilCards.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilCards.Images.SetKeyName(0, "2_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(1, "2_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(2, "2_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(3, "2_Spades.bmp");
+            this.ilCards.Images.SetKeyName(4, "3_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(5, "3_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(6, "3_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(7, "3_Spades.bmp");
+            this.ilCards.Images.SetKeyName(8, "4_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(9, "4_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(10, "4_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(11, "4_Spades.bmp");
+            this.ilCards.Images.SetKeyName(12, "5_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(13, "5_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(14, "5_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(15, "5_Spades.bmp");
+            this.ilCards.Images.SetKeyName(16, "6_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(17, "6_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(18, "6_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(19, "6_Spades.bmp");
+            this.ilCards.Images.SetKeyName(20, "7_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(21, "7_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(22, "7_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(23, "7_Spades.bmp");
+            this.ilCards.Images.SetKeyName(24, "8_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(25, "8_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(26, "8_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(27, "8_Spades.bmp");
+            this.ilCards.Images.SetKeyName(28, "9_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(29, "9_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(30, "9_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(31, "9_Spades.bmp");
+            this.ilCards.Images.SetKeyName(32, "10_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(33, "10_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(34, "10_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(35, "10_Spades.bmp");
+            this.ilCards.Images.SetKeyName(36, "Ace_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(37, "Ace_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(38, "Ace_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(39, "Ace_Spades.bmp");
+            this.ilCards.Images.SetKeyName(40, "Jack_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(41, "Jack_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(42, "Jack_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(43, "Jack_Spades.bmp");
+            this.ilCards.Images.SetKeyName(44, "Queen_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(45, "Queen_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(46, "Queen_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(47, "Queen_Spades.bmp");
+            this.ilCards.Images.SetKeyName(48, "King_Clubs.bmp");
+            this.ilCards.Images.SetKeyName(49, "King_Diamonds.bmp");
+            this.ilCards.Images.SetKeyName(50, "King_Hearts.bmp");
+            this.ilCards.Images.SetKeyName(51, "King_Spades.bmp");
+            this.ilCards.Images.SetKeyName(52, "Joker_Black.bmp");
+            this.ilCards.Images.SetKeyName(53, "Joker_Red.bmp");
+            this.ilCards.Images.SetKeyName(54, "Backface_Blue.bmp");
+            this.ilCards.Images.SetKeyName(55, "Backface_Red.bmp");
+            // 
+            // lbCards
+            // 
+            this.lbCards.FormattingEnabled = true;
+            this.lbCards.Location = new System.Drawing.Point(12, 12);
+            this.lbCards.Name = "lbCards";
+            this.lbCards.Size = new System.Drawing.Size(181, 290);
+            this.lbCards.TabIndex = 0;
+            this.lbCards.SelectedIndexChanged += new System.EventHandler(this.lbCards_SelectedIndexChanged);
+            // 
+            // pbSelectedCard
+            // 
+            this.pbSelectedCard.Location = new System.Drawing.Point(218, 109);
+            this.pbSelectedCard.Name = "pbSelectedCard";
+            this.pbSelectedCard.Size = new System.Drawing.Size(101, 125);
+            this.pbSelectedCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSelectedCard.TabIndex = 1;
+            this.pbSelectedCard.TabStop = false;
+            this.pbSelectedCard.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbSelectedCard_MouseDown);
+            this.pbSelectedCard.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbSelectedCard_MouseMove);
+            this.pbSelectedCard.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbSelectedCard_MouseUp);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(446, 278);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(93, 24);
+            this.btnShow.TabIndex = 2;
+            this.btnShow.Text = "Show Cards";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 316);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.pbSelectedCard);
+            this.Controls.Add(this.lbCards);
+            this.Name = "Form1";
+            this.Text = "Cards";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelectedCard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList ilCards;
+        private System.Windows.Forms.ListBox lbCards;
+        private System.Windows.Forms.PictureBox pbSelectedCard;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ InClass5-15-2018/Card Loader/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Card_Loader
+{
+    public partial class Form1 : Form
+    {
+        Point location = Point.Empty;
+        CardFiles[] allCards = new CardFiles[56];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int c = 0;
+            foreach (string card in ilCards.Images.Keys)
+            {
+                string cardName = card.Replace("_", " of ");
+                cardName = cardName.Replace(".bmp", "");
+                CardFiles info = new CardFiles(cardName, card);
+                allCards[c] = info;
+                lbCards.Items.Add(allCards[c].name);
+                c++;
+            }
+        }
+
+        private void lbCards_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            int index = 0;
+            foreach (CardFiles item in allCards)
+            {
+                if(lbCards.SelectedItem.ToString() == item.name.ToString())
+                {
+                    foreach (string card in ilCards.Images.Keys)
+                    {
+                        
+                        if(card == item.file)
+                        {
+                            pbSelectedCard.Image = ilCards.Images[index];
+                            pbSelectedCard.SizeMode = PictureBoxSizeMode.StretchImage;
+                            break;
+                        }
+                        index++;
+                    }
+
+                    
+                }
+            }
+            
+            //pbSelectedCard.Image = ilCards.Images.
+        }
+
+        private void pbSelectedCard_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                location = new Point(e.X, e.Y);
+            }
+        }
+
+        private void pbSelectedCard_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (location != Point.Empty)
+            {
+                Point newLocation = pbSelectedCard.Location;
+                newLocation.X += e.X - location.X;
+                newLocation.Y += e.Y - location.Y;
+                pbSelectedCard.Location = newLocation;
+            }
+        }
+
+        private void pbSelectedCard_MouseUp(object sender, MouseEventArgs e)
+        {
+            location = Point.Empty;
+        }
+
+        class CardFiles
+        {
+            string cardName;
+            string cardFile;
+
+            public CardFiles(string name, string file)
+            {
+                cardName = name;
+                cardFile = file;
+            }
+
+            public string name
+            {
+                get { return cardName; }
+                set { cardName = value; }
+           
+            }
+
+            public string file
+            {
+                get { return cardFile; }
+                set { cardFile = value; }
+
+            }
+        }
+
+        List<PictureBox> picList = new List<PictureBox>();
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            
+
+            int leftPos = 340;
+            int topPos = 12;
+            int row = 1;
+            for (int index = 0; index < ilCards.Images.Count; index++)
+            {
+                PictureBox newCard = new PictureBox();
+                newCard.Size = new Size(50, 70);
+                newCard.Location = new Point(leftPos, topPos);
+                leftPos += 25;
+                if (row > 9)
+                {
+                    topPos += 35;
+                    leftPos -= 250;
+                    row = 0;
+                }
+                newCard.Image = ilCards.Images[index];
+                this.Controls.Add(newCard);
+                newCard.BringToFront();
+                newCard.MouseDown += new MouseEventHandler(picMouseDown);
+                newCard.MouseMove += new MouseEventHandler(picMouseMove);
+                newCard.MouseUp += new MouseEventHandler(picMouseUp);
+                picList.Add(newCard);
+                
+
+                row++;
+            }
+        }
+        int x = 0;
+        int y = 0;
+        bool drag = false;
+        private void picMouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+            drag = true;
+        }
+
+        private void picMouseMove(object sender, MouseEventArgs e)
+        {
+            if(drag)
+            {
+                PictureBox pb = (PictureBox)sender;
+
+                pb.Top += e.Y - y;
+                pb.Left += e.X - x;
+                pb.BringToFront();
+            }
+        }
+
+        private void picMouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
+        }
+        
+    }
+}
